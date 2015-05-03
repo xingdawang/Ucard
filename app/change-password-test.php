e0f994edbc4076940652f8f0c0deaba2b74ff246
Xingda Wang <wxd598113636@gmail.com>
2015-05-03 14:06:01 +0100

    Add change password interface

---
+++ app/change-password-test.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+
+<html>
+<head>
+    <title>Page Title</title>
+</head>
+
+<body>
+
+    <form action="change-password.php" method="get" onsubmit="return checkForm(this);">
+        <hr>
+            Uuid: <input type="text" name="uuid"><br>
+	    Password: <input type="password" name="password"><br>
+            Password Confirm: <input type="password" name="passwordConfirm"><br>
+	    <input type="submit">
+        <hr>
+        <script type="text/javascript">
+            
+            // Password must be at least 6 length with upper and lower charaters
+            function checkPassword(str) {
+                var re = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,}$/;
+                return re.test(str);
+            }
+            
+            function checkForm(form) {
+                
+                // Password & PasswordConfirm should be same
+                if(form.password.value != "" && form.password.value == form.passwordConfirm.value) {
+                    if(!checkPassword(form.password.value)) {
+                        alert("Error: The password must contain 6 number, upper & lower characters!");
+                        form.password.focus();
+                        return false;
+                    }
+                } else {
+                    alert("Error: Password do not match");
+                    form.passwordConfirm.focus();
+                    return false;
+                }
+                return true;
+            }
+        </script>
+    </form>
+
+</body>
+</html>
---
+++ app/change-password.php
@@ -0,0 +1,37 @@
+<?php
+    /**
+     *  @author Xingda Wang <wxd598113636@gmail.com>
+     *  @since 1.0
+     *  @date 3rd May 2015
+     */
+    
+    $password = $_GET['password'];
+    $uuid = $_GET['uuid'];
+    $json_data = "";
+    $json_message = "";
+    $json_code = "";
+    
+    $password = md5(md5($password));
+    // Connect to server and select databse.
+    include"connectDB.php";
+    $tbl_name="userinfo"; // Table name
+    $sql = "SELECT * FROM $tbl_name WHERE uuid = '$uuid'";
+    $result = mysql_query($sql);
+    $number = mysql_num_rows($result);
+
+    
+    if($number == 1) {
+        $sql = "UPDATE $tbl_name SET password = '$password' WHERE uuid = '$uuid'";
+        $result = mysql_query($sql);
+        $json_code = 1000;
+        $json_message = "Password change succeed";
+    }else {
+        $json_code = 28;
+        $json_message = "User id is not found";
+    }
+    mysql_close();
+    
+    $array = Array('code'=>$json_code, 'message'=>$json_message, 'data'=>$json_data);
+    die(json_encode($array));
+
+?>
