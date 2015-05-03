3c1e953fe19a6678ff2989ea0ec8eeed12213730
Xingda Wang <wxd598113636@gmail.com>
2015-05-03 15:18:40 +0100

    Add postcard-receiving-list interface

---
+++ app/postcard-receiving-list-test.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+
+<html>
+<head>
+    <title>Page Title</title>
+</head>
+
+<body>
+
+    <form action="postcard-receiving-list.php" method="get">
+            Uuid: <input type="text" name="uuid"><br>
+	    <input type="submit">
+    </form>
+
+</body>
+</html>
+<?php
+
+?>
---
+++ app/postcard-receiving-list.php
@@ -0,0 +1,40 @@
+<?php
+    /**
+     *  @author Xingda Wang <wxd598113636@gmail.com>
+     *  @since 1.0
+     *  @date 3rd May 2015
+     */
+    
+    $uuid = $_GET['uuid'];
+    $json_data = "";
+    $json_message = "";
+    $json_code = "";
+    
+    $password = md5(md5($password));
+    // Connect to server and select databse.
+    include"connectDB.php";
+    $tbl_name="userinfo"; // Table name
+    $sql = "SELECT * FROM $tbl_name WHERE uuid = '$uuid'";
+    $result = mysql_query($sql);
+    $number = mysql_num_rows($result);
+    
+    if($number == 1) {
+        $tbl_name = "receiver";
+        $sql = "SELECT * FROM $tbl_name WHERE uuid = '$uuid'";
+        $result = mysql_query($sql);
+        $number = mysql_num_rows($result);
+        $json_code = 1000;
+        $json_message = "Receiving list returned";
+        while($row = mysql_fetch_array($result)){
+            $json_data[] = $row['postcard_uid'];
+        }
+    }else {
+        $json_code = 31;
+        $json_message = "User id is not found";
+    }
+    mysql_close();
+    
+    $array = Array('code'=>$json_code, 'message'=>$json_message, 'data'=>$json_data);
+    die(json_encode($array));
+
+?>
