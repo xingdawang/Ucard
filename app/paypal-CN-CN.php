e96280e11b1473dfe0f2b750d36d8114573999ba
Xingda Wang <wxd598113636@gmail.com>
2015-05-29 01:01:13 +0800

    change paypal button outlook

--- app/paypal-CN-CN.php
+++ app/paypal-CN-CN.php
@@ -10,12 +10,12 @@
     <?php
         $id = $_GET['record_id'];
     ?>
-    
+
     <form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
         <input type="hidden" name="cmd" value="_s-xclick">
         <input type="hidden" name="hosted_button_id" value="MTWZW47QLHE7W">
         <input type="hidden" name="custom" value='<?php echo $id?>'>
-        <input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_buynowCC_LG.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!">
+        <input type="image" src="http://212.111.43.104/paypal-buttons/btn.png" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!">
         <img alt="" border="0" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width="1" height="1">
     </form>
 
--- app/paypal-IE-IE.php
+++ app/paypal-IE-IE.php
@@ -15,9 +15,10 @@
         <input type="hidden" name="cmd" value="_s-xclick">
         <input type="hidden" name="hosted_button_id" value="ABVWGHMGER3NS">
         <input type="hidden" name="custom" value='<?php echo $id?>'>
-        <input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_buynowCC_LG.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!">
+        <input type="image" src="http://212.111.43.104/paypal-buttons/btn.png" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!">
         <img alt="" border="0" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width="1" height="1">
     </form>
+
     
 </body>
 </html>
