c848f38d13d4646f3d1c4b45d1d7e078270a0c40
Xingda Wang <wxd598113636@gmail.com>
2015-06-01 10:35:49 +0800

    get empty postcard id

---
+++ app/get-postcard-id.php
@@ -0,0 +1,22 @@
+<?php
+
+    /**
+     *  @author Xingda Wang <wxd598113636@gmail.com>
+     *  @since 1.0
+     *  @date 1st Jun 2015
+     *  Return empty postcard id
+     */
+    
+    $postcard_uid = uniqid();
+    // Connect to database
+    include "connectDB.php";
+    $tbl_name = "postcard";
+    $sql = "INSERT INTO $tbl_name (postcard_uid) VALUES ('$postcard_uid')";
+    mysql_query($sql);
+    mysql_close();
+    $json_code = 1000;
+    $json_message = 'Empty postcard generated';
+    $json_data = $postcard_uid;
+    $array = Array('code'=>$json_code, 'message'=>$json_message, 'data'=>$json_data);
+    die(json_encode($array));
+?>
